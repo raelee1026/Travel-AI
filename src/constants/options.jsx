1637ad0e7c0626b13b8ed8de0b37c6ba024fec74
raelee1026 <yylee1026@gmail.com>
2025-02-15 00:43:51 +0800

    feat: frontend

---
+++ src/constants/options.jsx
@@ -0,0 +1,56 @@
+export const SelectTravelesList=[
+    {
+        id:1,
+        title:'Solo Traveler',
+        desc:'Traveling alone can be a great way to explore a new destination. You can set your own pace and do exactly what you want to do. You can also meet new people and make new friends along the way.',
+        icon:'→',
+        people:'1'
+    },
+    {
+        id:2,
+        title:'Couple Traveler',
+        desc:'Traveling as a couple can be a great way to strengthen your relationship and create lasting memories together. You can explore new places, try new things, and enjoy quality time together.',
+        icon:'😂',
+        people:'2'
+    },
+    {
+        id:3,
+        title:'Family Traveler',
+        desc:'Traveling with your family can be a great way to spend quality time together and create lasting memories. You can explore new places, try new things, and bond over shared experiences.',
+        icon:'😂',
+        people:'3 to 5 people'
+    },
+    {
+        id:4,
+        title:'Friends Traveler',
+        desc:'Traveling with friends can be a fun and exciting way to explore new destinations. You can share new experiences, try new things, and create lasting memories together.',
+        icon:'👫',
+        people:'5 to 10 people'
+    },
+]
+
+export const SelectBudgetOptions=[ 
+    {
+        id:1,
+        title:'Cheap',
+        desc:'You can find affordable accommodations, eat cheaply, and take advantage of free or low-cost activities.',
+        icon:'💵',
+        budget:'$'
+    },
+    {
+        id:2,
+        title:'Mid-Range',
+        desc:'You can stay in comfortable accommodations, eat well, and enjoy a range of activities without spending a fortune.',
+        icon:'💰',
+        budget:'$$'
+    },
+    {
+        id:3,
+        title:'Luxury',
+        desc:'You can stay in luxurious accommodations, eat at top restaurants, and enjoy exclusive',
+        icon:'💸',
+        budget:'$$$'
+    }
+]
+
+export const AI_PROMPT='Generate Travel Plan for Location: {location} for {days} days for {people} people with {budget} budget'
